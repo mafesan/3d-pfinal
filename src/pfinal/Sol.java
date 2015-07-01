f847cbdd1a5a665bc8693a0f11770fabc84da9f2
miguel angel fernandez sanchez <maferna@epsilon38.aulas.gsyc.es>
2015-07-01 19:34:51 +0200

    Añadidos 2 objetos

---
+++ src/pfinal/Sol.java
@@ -0,0 +1,62 @@
+package pfinal;
+
+import Utils.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+
+
+public class Sol extends Dibujable {
+
+    private int uniModel;
+    private int uNMatrixAttribute;
+
+    private final SphereModel sphereModel = new SphereModel(20);
+
+    public void prepararBuffers(OpenGLHelper openGLHelper) {
+
+	        // --------------------- VERTICES POSITIONS --------------------------//
+	        int vbo_v = sphereModel.createVerticesBuffer();
+
+	        int posAttrib = openGLHelper.getShaderProgram().getAttributeLocation("aVertexPosition");
+	        glEnableVertexAttribArray(posAttrib);
+	        glBindBuffer(GL_ARRAY_BUFFER, vbo_v);
+	        glVertexAttribPointer(posAttrib, 3, GL_FLOAT, false, 0, 0);
+
+	        // ----------------------- NORMALS DATA -------------------------------//
+	        int vbo_n = sphereModel.createVerticesBuffer();
+
+	        int vertexNormalAttribute = openGLHelper.getShaderProgram().getAttributeLocation("aVertexNormal");
+	        glEnableVertexAttribArray(vertexNormalAttribute);
+	        glBindBuffer(GL_ARRAY_BUFFER, vbo_n);
+	        glVertexAttribPointer(vertexNormalAttribute, 3, GL_FLOAT, false, 0, 0);
+
+
+	        
+	        // ----------------------- ELEMENTS INDICES --------------------------//
+	        int ebo = sphereModel.createIndicesBuffer();
+
+	        uniModel = openGLHelper.getShaderProgram().getUniformLocation("model");
+
+	        uNMatrixAttribute = openGLHelper.getShaderProgram().getUniformLocation("uNMatrix");
+	    }
+
+	    @Override
+	    public void dibujar(OpenGLHelper openGLHelper) {
+	        /* Render a model */
+	        drawSomeModel(openGLHelper);
+	    }
+
+	    private void drawSomeModel(OpenGLHelper openGLHelper) {
+	        /* Render X, Y, Z */
+	        Matrix4f model = Matrix4f.translate(0, 5, -2.2f);
+	        glUniformMatrix4(uniModel, false, model.getBuffer());
+
+	        Matrix3f normalMatrix = model.multiply(openGLHelper.getViewMatrix()).toMatrix3f().invert();
+	        normalMatrix.transpose();
+	        glUniformMatrix3(uNMatrixAttribute, false, normalMatrix.getBuffer());
+
+	        glDrawElements(GL_TRIANGLES, sphereModel.getIndicesLength(), GL_UNSIGNED_INT, 0);
+	    }
+
+}
